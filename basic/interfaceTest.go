94b5cb9ef943f8d7c3080db7d741f1db13489eba
lipenghui2 <lipenghui2@vipkid.com.cn>
2020-09-10 01:12:09 +0800

    interface

---
+++ basic/interfaceTest.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+/*------------------- 隐式类型转换 -------------------*/
+type TestStruct struct{}
+
+func NilOrNot(v interface{}) bool {
+	return v == nil
+}
+
+func main() {
+	var s *TestStruct
+	//在类型转换时，*TestStruct 类型会转换成 interface{} 类型，
+	//转换后的变量不仅包含转换前的变量，还包含变量的类型信息 TestStruct，所以转换后的变量与 nil 不相等。
+	fmt.Println(s == nil)    // #=> true
+	fmt.Println(NilOrNot(s)) // #=> false
+}
